fb7695ac055d34c5d83202058644ca14ac538f89
Wahyuga <tawahyuga17@gmail.com>
2025-11-05 14:43:38 +0700

    Fix onTap di main Menu button add news

--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -29,7 +29,7 @@ class ItemCard extends StatelessWidget {
               ),
             );
           // Navigate ke route yang sesuai (tergantung jenis tombol)
-          if (item.name == "Tambah Berita") {
+          if (item.name == "Add News") {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => NewsFormPage()),
